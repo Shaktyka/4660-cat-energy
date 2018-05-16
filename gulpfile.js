de26aff61d15a80a51d96f130d4d90e3643940ec
Elena <k-i-s-u-n-j-a@list.ru>
2018-05-16 10:02:41 +0500

    Переименование иконок и правка таска в галп, чтобы в спрайт собирались только нужные svg-иконки

--- gulpfile.js
+++ gulpfile.js
@@ -62,7 +62,7 @@ gulp.task("webp", function() {
 });
 
 gulp.task("sprite", function() {
-  return gulp.src("source/img/icon-*.svg")
+  return gulp.src("source/img/sprite-*.svg")
     .pipe(svgstore({
 		inlineSvg: true
 	}))

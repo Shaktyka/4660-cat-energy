06bd985a83b26fa402ce7d9974029abb52ab0c44
Elena <k-i-s-u-n-j-a@list.ru>
2018-05-20 17:24:32 +0500

    Оформление и ПП формы десктоп

--- source/js/navopenclose.js
+++ source/js/navopenclose.js
@@ -1,4 +1,5 @@
-﻿var navMain = document.querySelector(".main-nav");
+﻿//Скрипт для открытия/закрытия меню модальной версии
+var navMain = document.querySelector(".main-nav");
 var navToggle = document.querySelector(".main-nav__toggle");
 navMain.classList.remove("main-nav--nojs");
 navToggle.addEventListener("click", function() {

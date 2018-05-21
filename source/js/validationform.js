ce9d2d5246e399d03862aae0ae823fae16de49b7
Elena <k-i-s-u-n-j-a@list.ru>
2018-05-21 14:47:01 +0500

    Дополнение файлов скриптов, правки по warnings. Фиксы багов стилизации в IE

--- source/js/validationform.js
+++ source/js/validationform.js
@@ -1,4 +1,4 @@
-﻿//Валидация формы на странице подбора программы
+﻿"use strict";
 
 var form = document.querySelector("form");
 var catName = form.querySelector("[name=cat-name-field]");
@@ -8,24 +8,22 @@ var phone = form.querySelector("[name=phone-field]");
 
 var isStorageSupport = true;
 var storage = "";
-  
+
 try {
   storage = localStorage.getItem("email");
-  } catch (err) {
-    isStorageSupport = false;
-  }
+} catch (err) {
+  isStorageSupport = false;
+}
 
 form.addEventListener("submit", function (evt) {
   if (!catName.value || !catWeight.value || !email.value || !phone.value) {
-	form.classList.add("input-error");
+    form.classList.add("input-error");
     evt.preventDefault();
     console.log("Введите, пожалуйста, имя кота, его вес, ваш e-mail и номер телефона для связи.");
   } else {
-	  form.classList.remove("input-error");
-	  if (isStorageSupport) {
-        localStorage.setItem("email", email.value);
-	  }
+    form.classList.remove("input-error");
+    if (isStorageSupport) {
+      localStorage.setItem("email", email.value);
+    }
   }
 });
-
-  

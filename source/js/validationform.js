e792cb6ada4a20e50c98a354bf372fe69af68c32
Elena <k-i-s-u-n-j-a@list.ru>
2018-05-28 17:38:34 +0500

    Удаление лишних комментариев, добавление нужных

--- source/js/validationform.js
+++ source/js/validationform.js
@@ -6,16 +6,61 @@ var catWeight = form.querySelector("[name=cat-weight-field]");
 var email = form.querySelector("[name=email-field]");
 var phone = form.querySelector("[name=phone-field]");
 
-form.addEventListener("submit", function (evt) {
-  if (!catName.value || !catWeight.value || !email.value || !phone.value) {
-  evt.preventDefault();
-  form.classList.add("error");
-  email.classList.add("error");
-  phone.classList.add("error");
-  console.log("Введите, пожалуйста, имя кота, его вес, ваш e-mail и номер телефона для связи.");
+var validateForm = function(evt) {
+  var hasError = false;
+
+  if (catName.value === "") {
+    hasError = true;
+    catName.classList.add("error");
   } else {
-  form.classList.remove("error");
-  email.classList.remove("error");
-  phone.classList.remove("error");
+    catName.classList.remove("error");
   }
+
+  if (catWeight.value  === "") {
+    hasError = true;
+    catWeight.classList.add("error");
+  } else {
+    catWeight.classList.remove("error");
+  }
+
+  if (email.value  === "") {
+    hasError = true;
+    email.classList.add("error");
+  } else {
+    email.classList.remove("error");
+  }
+
+  if (phone.value  === "") {
+    hasError = true;
+    phone.classList.add("error");
+  } else {
+    phone.classList.remove("error");
+  }
+
+  return hasError;
+}
+
+form.addEventListener("submit", function(evt) {
+  var isFormNotValid = validateForm();
+
+  if(isFormNotValid) {
+    evt.preventDefault();
+    console.log("Введите, пожалуйста, имя кота, его вес, ваш e-mail и номер телефона для связи.");
+  }
+});
+
+catName.addEventListener('change', function() {
+  validateForm();
+});
+
+catWeight.addEventListener('change', function() {
+  validateForm();
+});
+
+email.addEventListener('change', function() {
+  validateForm();
+});
+
+phone.addEventListener('change', function() {
+  validateForm();
 });
